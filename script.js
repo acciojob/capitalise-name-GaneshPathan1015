642b85ccbbe7bdfeb18fd12dd57be0ff534cb75c
Ganesh Pathan <118661019+GaneshPathan1015@users.noreply.github.com>
2023-12-29 15:02:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
 //your JS code here. If required.
 const fname = document.getElementById("fname");
-let str = fname.innertext;
-str.toUpparCase();
-fname.innertext = str;
+fname.value = fname.value.toUpperCase();
